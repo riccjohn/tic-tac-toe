0f6f7ea3e25904f3f4fc33847af20473cd3ba3a8
John <john@PotatoBeard.local>
2019-04-09 13:48:16 -0500

    feat: Add tests to check some win conditions, and implement code to pass them.
    Add tests to make sure player cannot overwrite an existing marker, and that the game will detect a
    win by row or column. Need to add a check for diagonal wins, as well as ties.

--- src/tests/tic-tac-toe.test.ts
+++ src/tests/tic-tac-toe.test.ts
@@ -2,20 +2,48 @@ import Game from "../tic-tac-toe";
 
 describe("Tic-tac-toe", () => {
   let game: Game;
+
   beforeEach(() => {
     game = new Game();
   });
+
   it("starts with an empty board", () => {
     const board = [["", "", ""], ["", "", ""], ["", "", ""]];
     expect(game.board).toEqual(board);
   });
-  it("places an X on the board on X's turn", () => {
+
+  it("places an X on the board on X's t urn", () => {
     game.place(0, 0);
     expect(game.board[0][0]).toEqual("X");
   });
+
   it("switches players every turn", () => {
     game.place(0, 0);
     game.place(0, 1);
     expect(game.board[0][1]).toEqual("O");
+  });
+
+  it("will not overwrite a marker where one already exists", () => {
+    game.place(0, 0);
+    game.place(0, 0);
+    expect(game.board[0][0]).toBe("X");
+  });
+
+  it("will end the game when there are 3 X's in a row", () => {
+    game.place(0, 0); // X
+    game.place(2, 0); // O
+    game.place(0, 1); // X
+    game.place(2, 1); // O
+    game.place(0, 2); // X
+    expect(game.winner).toEqual("X");
+  });
+
+  it("will end the game when there are 3 X's in a column", () => {
+    game.place(0, 0);
+    game.place(0, 2);
+    game.place(1, 0);
+    game.place(0, 1);
+    game.place(2, 0);
+    expect(game.winner).toEqual("X");
   });
 });
--- src/tic-tac-toe.ts
+++ src/tic-tac-toe.ts
@@ -5,9 +5,12 @@ type Board = Row[];
 class Game {
   public board: Board;
   private currentPlayer: string;
+  public winner: string;
+
   constructor() {
     this.currentPlayer = "X";
     this.board = [["", "", ""], ["", "", ""], ["", "", ""]];
+    this.winner = "";
   }
 
   switchPlayers() {
@@ -16,8 +19,38 @@ class Game {
   }
 
   place(x: number, y: number) {
-    this.board[x][y] = this.currentPlayer;
-    this.switchPlayers();
+    if (this.board[x][y] === "") {
+      this.board[x][y] = this.currentPlayer;
+      this.checkGameStatus();
+      this.switchPlayers();
+    }
+  }
+
+  checkRow(rowNumber: number) {
+    if (this.board[rowNumber][0] === this.currentPlayer) {
+      if (
+        this.board[rowNumber][1] === this.currentPlayer &&
+        this.board[rowNumber][2] === this.currentPlayer
+      )
+        this.winner = this.currentPlayer;
+    }
+  }
+
+  checkColumn(colNumber: number) {
+    if (this.board[0][colNumber] === this.currentPlayer) {
+      if (
+        this.board[1][colNumber] === this.currentPlayer &&
+        this.board[2][colNumber] === this.currentPlayer
+      )
+        this.winner = this.currentPlayer;
+    }
+  }
+
+  checkGameStatus() {
+    for (let i = 0; i < this.board.length; i++) {
+      this.checkRow(i);
+      this.checkColumn(i);
+    }
   }
 }
 
