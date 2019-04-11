9889e9955cb9b02213ee24784d5c3fe5102fc066
riccjohn@gmail.com <riccjohn@gmail.com>
2019-04-11 15:26:19 -0500

    refactor: Remove commented out lines

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,6 @@ module.exports = {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/"
-    // hotOnly: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devtool: "source-map"
