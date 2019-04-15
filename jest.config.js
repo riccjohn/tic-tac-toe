cf19072ba2ec457a795a6e38d57e9125e2b3bd8b
riccjohn@gmail.com <riccjohn@gmail.com>
2019-04-15 14:24:36 -0500

    chore: WIP: Install enzyme, get jest working with .tsx files

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   testEnvironment: "node",
-  testMatch: ['**/__tests__/*\.(test|spec)\.(js|ts)'],
-  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/__tests__/*\.(test|spec)\.(js|ts|tsx)'],
+  moduleFileExtensions: ['ts', 'js', 'tsx'],
   transform: {
     '^.+\.(ts)$': 'ts-jest'
   }
