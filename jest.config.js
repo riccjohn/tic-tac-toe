7243300e9720b9944c9b1ba78e1d5c88709f6a26
riccjohn@gmail.com <riccjohn@gmail.com>
2019-04-15 17:03:38 -0500

    chore: Set up jest to work with Enzyme and .tsx files
    Set up Jest to be able to read .tsx files and install Enzyme and Enzyme Adapter

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,17 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   testEnvironment: "node",
-  testMatch: ['**/__tests__/*\.(test|spec)\.(js|ts|tsx)'],
-  moduleFileExtensions: ['ts', 'js', 'tsx'],
+  testMatch: ["**/__tests__/*\.(test|spec)\.(js|ts|tsx)"],
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
   transform: {
-    '^.+\.(ts)$': 'ts-jest'
-  }
+    "^.+\.(ts|tsx)$": "ts-jest",
+  },
+  setupFiles: ["<rootDir>/src/setupEnzyme.ts"],
 };
--- src/__tests__/GameBoard.test.tsx
+++ src/__tests__/GameBoard.test.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import { GameBoard } from '../components';
+import GameBoard from '../components/GameBoard';
 
 describe('GameBoard component', () => {
   it('should render without throwing an error', () => {
     const board = shallow(<GameBoard />);
-    expect(false).toBe(true);
+    expect(board.find('form').exists()).toBe(true);
   });
 });
