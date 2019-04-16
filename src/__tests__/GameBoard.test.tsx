ff3ada5551f12d38d5a774c8d06cee726a488dd2
riccjohn@gmail.com <riccjohn@gmail.com>
2019-04-16 14:31:32 -0500

    test: Add tests for GameBoard component to make sure it renders correct amount of squares

--- src/__tests__/GameBoard.test.tsx
+++ src/__tests__/GameBoard.test.tsx
@@ -1,10 +1,42 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import GameBoard from '../components/GameBoard';
+import { PlayerPiece } from '../PlayerPiece';
 
 describe('GameBoard component', () => {
-  it('should render without throwing an error', () => {
-    const board = shallow(<GameBoard />);
-    expect(board.find('form').exists()).toBe(true);
+  it('should render a table', () => {
+    const wrapper = shallow(<GameBoard />);
+    expect(wrapper.find('table#game-table').exists()).toBe(true);
+  });
+
+  it('should render a 9 squares by default', () => {
+    const wrapper = shallow(<GameBoard />);
+    const squares = wrapper.find('td');
+    expect(squares).toHaveLength(9);
+  });
+
+  it('should render 25 squares given a 5x5 array', () => {
+    const testBoard = [
+      [undefined, undefined, undefined, undefined, undefined],
+      [undefined, undefined, undefined, undefined, undefined],
+      [undefined, undefined, undefined, undefined, undefined],
+      [undefined, undefined, undefined, undefined, undefined],
+      [undefined, undefined, undefined, undefined, undefined],
+    ];
+
+    const wrapper = shallow(<GameBoard board={testBoard} />);
+    const squares = wrapper.find('.square');
+    expect(squares).toHaveLength(25);
+  });
+
+  it('should render a grid containing player pieces', () => {
+    const testBoard = [
+      [PlayerPiece.X, PlayerPiece.O, PlayerPiece.X],
+      [PlayerPiece.O, PlayerPiece.X, PlayerPiece.O],
+      [PlayerPiece.X, PlayerPiece.O, PlayerPiece.X],
+    ];
+    const wrapper = shallow(<GameBoard board={testBoard} />);
+    const squares = wrapper.find('.square');
+    expect(squares).toHaveLength(9);
   });
 });
