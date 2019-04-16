1be677edbf03be9373198a599c7760a0ec57af2b
riccjohn@gmail.com <riccjohn@gmail.com>
2019-04-16 15:05:26 -0500

    fix: Remove unnecessary component import left-over

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { TicTacToe, GameBoard } from './components';
+import { TicTacToe } from './components';
 import './App.css';
 
 render(<TicTacToe />, document.getElementById('root'));
