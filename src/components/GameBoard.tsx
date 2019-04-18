878ca635c8e7ea292695ecb11cf3627cfe80b315
riccjohn@gmail.com <riccjohn@gmail.com>
2019-04-18 14:16:04 -0500

    feat: Change to functional component, utilize player input method supplied by parent.
    Restructure component to be stateless functional component. Utilize parent method for dealing with
    player input, and set a default prop method for player input.

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,42 +1,45 @@
 import * as React from 'react';
 
-interface IBoardProps {
-  board: Board;
-}
+const GameBoard: React.SFC<IBoardProps> = props => {
+  const { data } = props;
 
-class GameBoard extends React.Component<IBoardProps> {
-  public static defaultProps = {
-    board: Array(3)
-      .fill(undefined)
-      .map(() => Array(3).fill(undefined)),
+  const handleClick = (row: number, col: number): any => {
+    const coords = { row, col };
+    props.handlePlayerInput(coords);
   };
 
-  public render() {
-    const { board } = this.props;
-    return (
-      <React.Fragment>
-        {board ? (
-          <div id='board'>
-            <table id='game-table'>
-              <tbody>
-                {board.map((row: Row, rowIdx: number) => (
-                  <tr key={rowIdx} className='row'>
-                    {row.map((square: Square, squareIdx: number) => (
-                      <td key={squareIdx} className='square'>
-                        {square || '•'}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ) : (
-          <h1>NO_BOARD</h1>
-        )}
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      {data ? (
+        <div id='board'>
+          <table id='game-table'>
+            <tbody>
+              {data.map((row: Row, rowIdx: number) => (
+                <tr key={rowIdx} className='row'>
+                  {row.map((square: Square, squareIdx: number) => (
+                    <td
+                      key={squareIdx}
+                      className='square'
+                      onClick={() => handleClick(rowIdx, squareIdx)}
+                    >
+                      {square || '•'}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <h1>NO_BOARD</h1>
+      )}
+    </React.Fragment>
+  );
+};
+
+GameBoard.defaultProps = {
+  handlePlayerInput: (coords: ICoords) =>
+    console.log('DEFAULT PROP', coords.row, coords.col),
+};
 
 export default GameBoard;
