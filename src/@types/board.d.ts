8e6b6003381f9aa0649389a2df9fd7a90822d3ab
riccjohn@gmail.com <riccjohn@gmail.com>
2019-04-18 14:05:01 -0500

    feat: Add interfaces for BoardProps and Coordinates
    Move interface type definition from GameBoard.tsx into type definition file, and create a new
    interface definition for Coordinates.

--- src/@types/board.d.ts
+++ src/@types/board.d.ts
@@ -2,3 +2,13 @@ type PlayerPiece = 'X' | 'O';
 type Square = PlayerPiece | undefined;
 type Row = Square[];
 type Board = Row[];
+
+interface IBoardProps {
+  data?: Board;
+  handlePlayerInput?: any;
+}
+
+interface ICoords {
+  row: number;
+  col: number;
+}
