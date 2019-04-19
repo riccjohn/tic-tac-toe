2090cf0c10d116d3d05b05dabdf3994e78af6ada
riccjohn@gmail.com <riccjohn@gmail.com>
2019-04-19 10:59:57 -0500

    refactor(GameBoard.tsx): Remove unnecessary React.Fragment

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,38 +1,34 @@
 import * as React from 'react';
 
 const GameBoard: React.SFC<IBoardProps> = props => {
-  const { data } = props;
+  const { data, handlePlayerInput } = props;
   const handleClick = (row: number, col: number): any => {
     const coords = { row, col };
-    props.handlePlayerInput(coords);
+    handlePlayerInput(coords);
   };
 
-  return (
-    <React.Fragment>
-      {data ? (
-        <div id='board'>
-          <table id='game-table'>
-            <tbody>
-              {data.map((row: Row, rowIdx: number) => (
-                <tr key={rowIdx} className='row'>
-                  {row.map((square: Square, colIdx: number) => (
-                    <td
-                      key={colIdx}
-                      className='square'
-                      onClick={() => handleClick(rowIdx, colIdx)}
-                    >
-                      {square || '•'}
-                    </td>
-                  ))}
-                </tr>
+  return data ? (
+    <div id='board'>
+      <table id='game-table'>
+        <tbody>
+          {data.map((row: Row, rowIdx: number) => (
+            <tr key={rowIdx} className='row'>
+              {row.map((square: Square, colIdx: number) => (
+                <td
+                  key={colIdx}
+                  className='square'
+                  onClick={() => handleClick(rowIdx, colIdx)}
+                >
+                  {square || '•'}
+                </td>
               ))}
-            </tbody>
-          </table>
-        </div>
-      ) : (
-        <h1>NO_BOARD</h1>
-      )}
-    </React.Fragment>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  ) : (
+    <h1>NO_BOARD</h1>
   );
 };
 
