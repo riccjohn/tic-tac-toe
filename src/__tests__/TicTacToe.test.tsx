447649a881cde08a5f2fec40a0bd2de3adcdfb7e
riccjohn@gmail.com <riccjohn@gmail.com>
2019-04-30 14:15:29 -0500

    test: Test that changing state to have an error triggers the Error component to render

--- src/__tests__/TicTacToe.test.tsx
+++ src/__tests__/TicTacToe.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render, mount } from 'enzyme';
 import TicTacToe from '../components/TicTacToe';
 
 describe('TicTacToe component', () => {
@@ -11,5 +11,20 @@ describe('TicTacToe component', () => {
   it('should render the BoardSizeForm component by default', () => {
     const wrapper = shallow(<TicTacToe />);
     expect(wrapper.find('BoardSizeForm').exists()).toBe(true);
+  });
+
+  it('will render the Error component if necessary', () => {
+    const wrapper = mount(<TicTacToe />);
+    expect(
+      wrapper.contains(
+        <p>Error: Invalid input. Please input a number from 3 - 10</p>
+      )
+    ).toBeFalsy();
+    wrapper.setState({ hasError: true });
+    expect(
+      wrapper.contains(
+        <p>Error: Invalid input. Please input a number from 3 - 10</p>
+      )
+    ).toBeTruthy();
   });
 });
