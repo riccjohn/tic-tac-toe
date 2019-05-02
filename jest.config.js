4f161af37d5b6446f283e55f8379b0ff8a0b8946
riccjohn@gmail.com <riccjohn@gmail.com>
2019-05-02 11:19:25 -0500

    chore: Add jest options to report pass/fail on code coverage

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,12 @@ module.exports = {
     "^.+\.(ts|tsx)$": "ts-jest",
   },
   setupFiles: ["<rootDir>/src/__tests__/setupEnzyme.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
