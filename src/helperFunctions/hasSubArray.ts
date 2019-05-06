5a910edbcf74e109f301e675b607f386570dbd66
riccjohn@gmail.com <riccjohn@gmail.com>
2019-05-06 14:20:21 -0500

    Add default value for the master array in case there is none

--- src/helperFunctions/hasSubArray.ts
+++ src/helperFunctions/hasSubArray.ts
@@ -1,4 +1,7 @@
-export const hasSubArray = (master: number[][], sub: number[]): boolean => {
+export const hasSubArray = (
+  master: number[][] = [[]],
+  sub: number[]
+): boolean => {
   for (const val of master) {
     if (val.toString() === sub.toString()) {
       return true;
