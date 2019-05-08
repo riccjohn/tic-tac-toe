db2f7e05d7130aea8f4b120c11c62cbbdfec58e1
riccjohn@gmail.com <riccjohn@gmail.com>
2019-05-08 11:39:51 -0500

    refactor: Update descriptions to reflect new function better

--- src/__tests__/contains.test.ts
+++ src/__tests__/contains.test.ts
@@ -1,13 +1,13 @@
 import { containsCoordinates } from '../helperFunctions/contains';
 
 describe('containsCoordinates', () => {
-  it('will find a sub-array in a master array', () => {
+  it('will find a coordinate object in a master array', () => {
     const array = [{ col: 0, row: 0 }, { col: 0, row: 1 }, { col: 0, row: 2 }];
     const obj = { col: 0, row: 0 };
     expect(containsCoordinates(array, obj)).toBe(true);
   });
 
-  it('will not send a false positive', () => {
+  it('will return false if master array does not contain element', () => {
     const array = [{ col: 0, row: 0 }, { col: 0, row: 1 }, { col: 0, row: 2 }];
     const obj = { col: 1, row: 1 };
     expect(containsCoordinates(array, obj)).toBe(false);
