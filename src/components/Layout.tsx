ee1b634eec3329664c16d4848ab5a41b74a153e2
riccjohn@gmail.com <riccjohn@gmail.com>
2019-05-13 14:36:00 -0500

    refactor: Destructure children off of props since it's the only thing being used

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,10 @@ import * as React from 'react';
 import FullWidthContainer from './atoms/FullWidthContainer';
 import ContentContainer from './atoms/ContentContainer';
 
-const Layout: React.SFC<{ children: JSX.Element[] }> = props => {
-  return (
-    <FullWidthContainer>
-      <ContentContainer>{props.children}</ContentContainer>
-    </FullWidthContainer>
-  );
-};
+const Layout: React.SFC<{ children: JSX.Element[] }> = ({ children }) => (
+  <FullWidthContainer>
+    <ContentContainer>{children}</ContentContainer>
+  </FullWidthContainer>
+);
 
 export default Layout;
