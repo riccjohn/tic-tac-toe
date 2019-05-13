fc58e8f7b9dbd7a0ab1bf49b0bb1d54dda76b1fa
riccjohn@gmail.com <riccjohn@gmail.com>
2019-05-13 15:29:28 -0500

    feat: Make board cells square and stay the same size regardless of input

--- src/components/atoms/Square.tsx
+++ src/components/atoms/Square.tsx
@@ -6,14 +6,16 @@ type SquareProps = {
 };
 
 const Square = styled.td`
-  padding: 2rem;
-  font-size: 2rem;
+  width: 4em;
+  height: 4em;
+  font-size: 2.25em;
   font-weight: bold;
   text-align: center;
   border: 4px solid ${props => props.theme.lightText};
   color: ${props => props.theme.lightText};
   background-color: ${props => props.theme.primary};
   transition: all 125ms ease-in-out;
+
   &:hover {
     background-color: ${props => props.theme.active};
   }
--- src/components/atoms/Table.tsx
+++ src/components/atoms/Table.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const Table = styled.table`
+  table-layout: fixed;
+  white-space: nowrap;
   border-collapse: collapse;
   align-self: center;
-  max-width: 400px;
-  min-width: 250px;
   margin-bottom: 1rem;
 `;
 
